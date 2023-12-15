258e4829f9fabb6fe952d2de4a045f6f8999138e
James Lawrence <jameslaw195@gmail.com>
2023-12-15 11:32:23 +0100

    move fetcher function to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const apiURL = "https://example-apis.vercel.app/api/art";
+
+const fetcher = async (apiURL) => {
+  const response = await fetch(apiURL);
+
+  if (!response.ok) {
+    const error = new Error("An error occured.");
+
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+  return response.json();
+};
 
 export default function App({ Component, pageProps }) {
+  const { data: art, error, isLoading } = useSWR(apiURL, fetcher);
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <>
+      <h1>{art.map((art) => art.name)}</h1>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
---
+++ pages/components/Spotlight/Spotlight.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Spotlight() {
+  return <>function to pick one art piece</>;
+}
