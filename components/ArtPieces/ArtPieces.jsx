8a4b11ed88643e6af16575c12b07f8833e100892
James Lawrence <jameslaw195@gmail.com>
2023-12-18 10:40:20 +0100

    add id and and return statement

--- components/ArtPieces/ArtPieces.jsx
+++ components/ArtPieces/ArtPieces.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import ArtPiecePreview from "../ArtPiecePreview/ArtPiecePreview";
 
 export default function ArtPieces({ artPiece }) {
+  console.log("this is art piece", artPiece);
   return (
     <>
-      {artPiece.map((artPiece) => {
+      {artPiece.map((artPiece) => (
         <ArtPiecePreview
+          key={artPiece.id}
           // image={artPiece.imageSource}
           name={artPiece.name}
           artist={artPiece.artist}
-        />;
-      })}
+        />
+      ))}
     </>
   );
 }
