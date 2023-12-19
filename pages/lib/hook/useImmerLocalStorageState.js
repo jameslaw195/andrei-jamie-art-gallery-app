d00fab08ce7ab6c2d03d933ff903944cf2124c0a
James Lawrence <jameslaw195@gmail.com>
2023-12-19 13:43:50 +0100

    fix immerstorage

--- pages/lib/hook/useImmerLocalStorageState.js
+++ pages/lib/hook/useImmerLocalStorageState.js
@@ -3,7 +3,7 @@ import { freeze } from "immer";
 import { useCallback } from "react";
 import useLocalStorageState from "use-local-storage-state";
 
-export function useImmerLocalStorageState(key, options) {
+export default function useImmerLocalStorageState(key, options) {
   const [value, setValue] = useLocalStorageState(key, {
     ...options,
     defaultValue: freeze(options.defaultValue),
